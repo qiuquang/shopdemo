64b4867ce9ff8788fb4fa78b2d058342a655c5ba
邱全 <qiuquan@qiuquandeMacBook-Pro.local>
2022-09-24 19:35:40 +0800

    搭建后台server

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -19,13 +19,9 @@
 				:key='index'
 			>
 				<view class='home-data'>
-					<Banner></Banner>
-					<Icons></Icons>
-					<Card cardTitle='热销爆品'></Card>
-					<Hot></Hot>
-					<Card cardTitle='推荐店铺'></Card>
-					<Shop></Shop>
-					<Card cardTitle='为您推荐'></Card>
+					<IndexSwiper></IndexSwiper>
+					<Recommend></Recommend>
+					<Card cardTitle='猜你喜欢'></Card>
 					<CommodityList></CommodityList>
 				</view>
 			</swiper-item>

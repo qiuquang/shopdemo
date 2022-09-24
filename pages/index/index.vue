3bff32dd643862228baa02450209fba2c835e134
邱全 <qiuquan@qiuquandeMacBook-Pro.local>
2022-09-24 14:23:03 +0800

    商品列表和单个商品组件封装

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,6 +3,7 @@
 		<IndexSwiper></IndexSwiper>
 		<Recommend></Recommend>
 		<Card cardTitle="猜你喜欢"></Card>
+		<CommodityList></CommodityList>
 	</view>
 </template>
 
@@ -10,9 +11,10 @@
 	import IndexSwiper from '@/components/index/IndexSwiper.vue'
 	import Recommend from '@/components/index/Recommend.vue'
 	import Card from '@/components/common/Card.vue'
+	import CommodityList from '@/components/common/CommodityList.vue'
 	
 	export default {
-		components: {IndexSwiper, Recommend, Card},
+		components: {IndexSwiper, Recommend, Card, CommodityList},
 		data() {
 			return {
 				title: 'Hello'

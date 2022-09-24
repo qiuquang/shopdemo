a4caf60cadf744065e336b0b66821a5219985e87
邱全 <qiuquan@qiuquandeMacBook-Pro.local>
2022-09-24 19:20:08 +0800

    修改内容高度

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,53 @@
 <template>
-	<view class="index">
-		<IndexSwiper></IndexSwiper>
+	<view class='index'>
+		
+		<scroll-view scroll-x="true" class='scroll-content' :scroll-into-view='scrollIntoIndex'>
+			<view
+				:id="'top'+index"
+				class='scroll-item'
+				v-for='(item,index) in topBar'
+				:key='index'
+				@tap='changeTab(index)'
+			>
+				<text :class='topBarIndex===index? "f-active-color":"f-color"'>{{item.name}}</text>
+			</view>
+		</scroll-view>
+		
+		<swiper  @change='onChangeTab' :current="topBarIndex" :style="'height:'+clentHeight+'px;'">
+			<swiper-item 
+				v-for='(item,index) in topBar'
+				:key='index'
+			>
+				<view class='home-data'>
+					<Banner></Banner>
+					<Icons></Icons>
+					<Card cardTitle='热销爆品'></Card>
+					<Hot></Hot>
+					<Card cardTitle='推荐店铺'></Card>
+					<Shop></Shop>
+					<Card cardTitle='为您推荐'></Card>
+					<CommodityList></CommodityList>
+				</view>
+			</swiper-item>
+		</swiper>
+		
+		
+		<!-- 推荐模版 -->
+		<!-- <IndexSwiper></IndexSwiper>
 		<Recommend></Recommend>
-		<Card cardTitle="猜你喜欢"></Card>
-		<CommodityList></CommodityList>
-		<Banner></Banner>
+		<Card cardTitle='猜你喜欢'></Card>
+		<CommodityList></CommodityList> -->
+		
+		
+		<!-- 其他模版：运动户外、美妆... -->
+		<!-- <Banner></Banner>
 		<Icons></Icons>
-		<Card cardTitle="热销爆品"></Card>
+		<Card cardTitle='热销爆品'></Card>
 		<Hot></Hot>
-		<Card cardTitle="推荐店铺"></Card>
+		<Card cardTitle='推荐店铺'></Card>
 		<Shop></Shop>
+		<Card cardTitle='为您推荐'></Card>
+		<CommodityList></CommodityList> -->
 		
 	</view>
 </template>
@@ -23,32 +61,75 @@
 	import Icons from '@/components/index/Icons.vue'
 	import Hot from '@/components/index/Hot.vue'
 	import Shop from '@/components/index/Shop.vue'
-	
-	
 	export default {
-		components: {
-			IndexSwiper, 
-			Recommend, 
-			Card, 
+		data() {
+			return {
+				//选中的索引
+				topBarIndex:0,
+				//顶栏跟随的索引id值
+				scrollIntoIndex:'top0',
+				//内容块的高度值
+				clentHeight:0,
+				//顶栏数据
+				topBar:[
+					{name:'推荐'},
+					{name:'运动户外'},
+					{name:'服饰内衣'},
+					{name:'鞋靴箱包'},
+					{name:'美妆个护'},
+					{name:'家居数码'},
+					{name:'食品母婴'}
+				]
+			}
+		},
+		components:{
+			IndexSwiper,
+			Recommend,
+			Card,
 			CommodityList,
 			Banner,
 			Icons,
 			Hot,
 			Shop
 		},
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
 		onLoad() {
-
+			
 		},
-		methods: {
-
+		onReady() {
+			let view = uni.createSelectorQuery().select(".home-data");
+			view.boundingClientRect(data => {
+			    this.clentHeight = data.height;
+			}).exec();
+			
+		},
+		methods:{
+			changeTab(index){
+				if(this.topBarIndex === index){
+					return ;
+				}
+				this.topBarIndex = index;
+				this.scrollIntoIndex = 'top'+index;
+			},
+			onChangeTab(e){
+				this.changeTab(e.detail.current);
+			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+.scroll-content{
+	width: 100%;
+	height: 80rpx;
+	white-space: nowrap;
+}
+.scroll-item{
+	display: inline-block;
+	padding:10rpx 30rpx;
+	font-size:32rpx;
+}
+.f-active-color{
+	padding:10rpx 0;
+	border-bottom:6rpx solid #49BDFB;
+}
 </style>
